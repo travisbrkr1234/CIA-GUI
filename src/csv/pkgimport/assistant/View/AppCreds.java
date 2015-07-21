f0d4cb56dfdedbdbdb072dad80f776b520074362
travisbrkr1234 <carlos.ochoa@infusionsoft.com>
2015-07-21 09:15:12 -0700

    added save settings

--- src/csv/pkgimport/assistant/View/AppCreds.java
+++ src/csv/pkgimport/assistant/View/AppCreds.java
@@ -5,11 +5,17 @@
  */
 package csv.pkgimport.assistant.View;
 
+import java.awt.Component;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import org.apache.xmlrpc.XmlRpcException;
 import org.supdev.ISClient;
 
@@ -41,6 +47,7 @@ public class AppCreds extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -57,6 +64,13 @@ public class AppCreds extends javax.swing.JFrame {
 
         jLabel3.setText("API Key:");
 
+        jButton1.setText("Save Settings...");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -71,14 +85,17 @@ public class AppCreds extends javax.swing.JFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(jLabel1)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel3)
-                                .addGap(18, 18, 18)
-                                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
                                 .addComponent(jLabel2)
                                 .addGap(18, 18, 18)
-                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(114, 114, 114))))
+                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel3)
+                                .addGap(18, 18, 18)
+                                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(114, 114, 114))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addGap(146, 146, 146))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -93,7 +110,9 @@ public class AppCreds extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 97, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 68, Short.MAX_VALUE)
                 .addComponent(jButton2)
                 .addContainerGap())
         );
@@ -115,11 +134,36 @@ public class AppCreds extends javax.swing.JFrame {
             Logger.getLogger(AppCreds.class.getName()).log(Level.SEVERE, null, ex);
         }
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        try {
+            appName = jTextField1.getText();
+            apiKey = jTextField2.getText();
+            System.err.println(appName + " " +  apiKey);
+            String content = "app name: " + appName + " API Key: " + apiKey;
+            
+            File file = new File("C:\\Users\\carlos.ochoa\\Desktop\\settingsFile.txt");
+            
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(content);
+            bw.close();
+            Component frame = null;
+            JOptionPane.showMessageDialog(frame, "You have saved your settings in: \n" + file);
+            
+    } catch (IOException e){
+        e.printStackTrace();
+    }
+    }//GEN-LAST:event_jButton1ActionPerformed
     protected void proceed() throws MalformedURLException, XmlRpcException {
   appName = jTextField1.getText();
   apiKey = jTextField2.getText();
   System.err.println(appName + " " +  apiKey);
-  
+
   try {
     ISClient thisclient = new ISClient(appName, apiKey);
     thisclient.printFirstContacts();
@@ -130,7 +174,6 @@ public class AppCreds extends javax.swing.JFrame {
   } catch (XmlRpcException e) {
     System.err.println("XmlRpcException Ya broke it" + e.getMessage());
   }
-  
 }
 
     /**
@@ -169,6 +212,7 @@ public class AppCreds extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
